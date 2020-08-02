6bb428a8d6b9e89e9448549f08032264770d98ff
Luca Hanel <lhanel@pm.me>
2020-08-02 17:46:21 +0200

    worked in the wishbone changes

--- load_store_unit/src/store_unit.sv
+++ load_store_unit/src/store_unit.sv
@@ -28,7 +28,7 @@ module store_unit
     input logic [31:0]                 data_i,
     input logic [3:0]                  we_i,
     output logic                       valid_o,
-    wb_master_bus_t                    wb_bus
+    wb_bus_t.master                    wb_bus
 );
 
 enum logic {IDLE, WRITE} CS, NS;
@@ -44,20 +44,20 @@ logic               wb_gnt_i;  // Bus granted by interconnect
 // local variables to wishbone bus (just dont want to rewrite everything ':D)
 // assign wb_dat_i     = wb_bus.wb_dat_i;
 // assign wb_tgd_i     = wb_bus.wb_tgd_i;
-assign wb_ack_i     = wb_bus.wb_ack_i;
+assign wb_ack_i     = wb_bus.wb_ack;
 // assign wb_err_i     = wb_bus.wb_err_i;
 // assign wb_rty_i     = wb_bus.wb_rty_i;
-assign wb_gnt_i     = wb_bus.wb_gnt_i;
-assign wb_bus.wb_sel_o     = wb_sel_o;
-assign wb_bus.wb_stb_o     = wb_stb_o;
-assign wb_bus.wb_we_o      = write_i;
-assign wb_bus.wb_lock_o    = 'b0;
-assign wb_bus.wb_cyc_o     = wb_cyc_o;
-assign wb_bus.wb_tgc_o     = 'b0;
-assign wb_bus.wb_dat_o     = data_i;
-assign wb_bus.wb_tgd_o     = 'b0;
-assign wb_bus.wb_adr_o     = addr_i;
-assign wb_bus.wb_tga_o     = 'b0;
+assign wb_gnt_i     = wb_bus.wb_gnt;
+assign wb_bus.wb_sel     = wb_sel_o;
+assign wb_bus.wb_stb     = wb_stb_o;
+assign wb_bus.wb_we      = write_i;
+assign wb_bus.wb_lock    = 'b0;
+assign wb_bus.wb_cyc     = wb_cyc_o;
+assign wb_bus.wb_tgc     = 'b0;
+assign wb_bus.wb_dat_ms     = data_i;
+assign wb_bus.wb_tgd_ms     = 'b0;
+assign wb_bus.wb_adr     = addr_i;
+assign wb_bus.wb_tga     = 'b0;
 
 // State machine for wb master
 always_comb
